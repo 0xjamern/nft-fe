20d7f56b25f6b920eaf98a2f6ccc2e910a5a6f77
0xjamern <jamesjo2019@gmail.com>
2023-11-23 12:38:01 +1100

    feat: update for pwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,8 +12,19 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Simple NFT mint frontend" />
+
         <title>NFT Mint and Listing</title>
         <link rel="icon" href="/favicon.ico"></link>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
       </Head>
       <ToastContainer
         position="top-right"
--- utils/unft.ts
+++ utils/unft.ts
@@ -6,21 +6,25 @@ import UNftAbi from "../abis/unft.json";
 const provider = new providers.JsonRpcProvider(RPC_URL);
 const UNftContract = new Contract(CONTRACTS.UNFT, UNftAbi, provider);
 
+// get required ETH value to mint
 export const getMintValue = async (): Promise<string> => {
   const mintValue = await UNftContract.mintValue();
   return utils.formatEther(mintValue);
 };
 
+// to fetch minted nft
 export const getCurrentId = async (): Promise<string> => {
   const currentId = await UNftContract.getCurrentId();
   return currentId.toString();
 };
 
+// to fetch nft collection name
 export const getNftName = async (): Promise<string> => {
   const nftName = await UNftContract.name();
   return nftName.toString();
 };
 
+// this function was added as of wrong uri set in contract
 export const formatUri = (uri: string): string => {
   return uri.includes("/ipfs/")
     ? uri
@@ -30,16 +34,19 @@ export const formatUri = (uri: string): string => {
       );
 };
 
+// to get tokenUri from the tokenId
 export const getTokenUri = async (tokenId: number): Promise<string> => {
   const tokenUri = (await UNftContract.tokenURI(tokenId)).toString();
   return formatUri(tokenUri);
 };
 
+// get owner address of certain tokenId
 export const getOwner = async (tokenId: number): Promise<string> => {
   const owner = await UNftContract.ownerOf(tokenId);
   return owner;
 };
 
+// function to mint the NFT
 export const mintNft = async (
   signer: Signer,
   valueAmt: string,
