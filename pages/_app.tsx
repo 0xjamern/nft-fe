1d62b222d9906837d9a98b135b62a69b78d162f5
naruto0913 <abe.sinjiro7@gmail.com>
2023-11-27 18:19:31 +1100

    feat: update with supported wallets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import Head from "next/head";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
-import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+import {
+  ThirdwebProvider,
+  ChainId,
+  embeddedWallet,
+  metamaskWallet,
+} from "@thirdweb-dev/react";
 
 import "../styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
@@ -33,6 +38,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       />
       <ThirdwebProvider
         activeChain={ChainId.Goerli}
+        supportedWallets={[embeddedWallet(), metamaskWallet()]}
         clientId={process.env.NEXT_PUBLIC_THIRDWEB_PROJECT_ID}
       >
         <AnyComponent {...pageProps} />
